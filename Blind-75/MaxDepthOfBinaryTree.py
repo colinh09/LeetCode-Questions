3f130b912caa69388a3be4f140fb0b98505667fa
colinh09 <70493957+colinh09@users.noreply.github.com>
2022-08-31 10:18:18 -0400

    Update and rename MaxDepthOfBinaryTree to MaxDepthOfBinaryTree.py

--- Blind-75/MaxDepthOfBinaryTree.py
+++ Blind-75/MaxDepthOfBinaryTree.py
@@ -10,6 +10,6 @@ class Solution:
         if not root:
             return 0
         #break the problem into sub problems. We want to keep exploring right and
-        #left subtrees for every trie, if null, return 0 as in the base case
+        #left subtrees for every set of three nodes, if null, return 0 as in the base case
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
             
