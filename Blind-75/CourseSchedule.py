f660c0fcd94a2f3ac83570d2622066c242bf41f9
colinh09 <70493957+colinh09@users.noreply.github.com>
2022-09-05 12:11:29 -0400

    Create CourseSchedule.py

---
+++ Blind-75/CourseSchedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # for every course initially, we want to map it to an empty list
+        hashMap = { i:[] for i in range(numCourses)}
+        # map each course to its prereq
+        for course, pre in prerequisites:
+            hashMap[course].append(pre)
+        # keep track of visited course nodes in a set
+        visited = set()
+        def dfs(course):
+            # if the course has already been visited, then there is a loop within the graph and it is impossible to take all courses
+            if course in visited:
+                return False
+            # if there are no prereqs for the course, then u can just take it lol
+            if hashMap[course] == []:
+                return True
+            # proceed to add the course to the visited set if edge cases aren't satisified
+            visited.add(course)
+            # preforming dfs, visiting every prereq for the course that we are visiting
+            for prereq in hashMap[course]:
+                if dfs(prereq) == False:
+                    return False
+            # we are done with the course, so we can remove it from the visited set
+            visited.remove(course)
+            # we know now that from this course, every course can be taken. There is no need to redo work if this node is visited again, so just set no prereqs to the course so it is automatically going to return true in future dfs calls. 
+            hashMap[course] = []
+            return True
+        # the graph can be connected. Therefore, we have to ensure that we are preforming dfs on every node.
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        return True
